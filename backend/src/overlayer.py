c87cf96060fb777fbde870296beb9ea0270d9fd4
mihirathale98 <athale.m@northeastern.edu>
2025-01-26 18:48:45 -0500

    updaed dockerfile req

--- backend/src/overlayer.py
+++ backend/src/overlayer.py
@@ -9,8 +9,8 @@ def overlay_wav_files(file_paths):
         combined = combined.overlay(sound)
     return combined
 
-file_paths = os.listdir("../demo_sounds")
-file_paths = [os.path.join("../demo_sounds", file_path) for file_path in file_paths]
-song = overlay_wav_files(file_paths)
+# file_paths = os.listdir("../demo_sounds")
+# file_paths = [os.path.join("../demo_sounds", file_path) for file_path in file_paths]
+# song = overlay_wav_files(file_paths)
 
-song.export("output.wav", format="wav")
+# song.export("output.wav", format="wav")
